116640c640e4227d6ed67f0708fc8a94a14373a6
Suumi <ksksme@gmail.com>
2021-08-04 18:34:36 +0900

    編集ルーティング追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,3 +10,5 @@ Route::get('board/index', [BoardController::class, 'index'])->name('board.index'
 Route::get('board/posts/{post}', [BoardController::class, 'show'])->name('board.show')->where('post', '[0-9]+');
 Route::get('board/create', [BoardController::class, 'create'])->name('board.create');
 Route::post('board/store', [BoardController::class, 'store'])->name('board.store');
+Route::get('board/posts/{post}/edit', [BoardController::class, 'edit'])->name('board.edit')->where('post', '[0-9]+');
+Route::patch('board/posts/{post}/update', [BoardController::class, 'update'])->name('board.update')->where('post', '[0-9]+');
