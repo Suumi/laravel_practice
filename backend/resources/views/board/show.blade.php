21cda7f5c3a3e0c2e042270586a0d79e31c0b430
Suumi <ksksme@gmail.com>
2021-08-04 18:33:05 +0900

    投稿編集機能
    * 部分更新処理のためpatchメソッド
    * bladeではPATCHを明示

--- backend/resources/views/board/show.blade.php
+++ backend/resources/views/board/show.blade.php
@@ -7,6 +7,9 @@
         <a href="{{ route('board.index') }}">戻る</a>
     </div>
 
-    <h1>{{ $post->title }}</h1>
+    <h1>
+        <span>{{ $post->title }}</span>
+        <a href="{{ route('board.edit', $post) }}">編集</a>
+    </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
 </x-layout>
