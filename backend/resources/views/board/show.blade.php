5104cb1ed451cc90c2045ae778c1d069fb23b098
Suumi <ksksme@gmail.com>
2021-08-04 21:21:48 +0900

    削除前にダイアログ表示

--- backend/resources/views/board/show.blade.php
+++ backend/resources/views/board/show.blade.php
@@ -10,11 +10,27 @@
     <h1>
         <span>{{ $post->title }}</span>
         <a href="{{ route('board.edit', $post) }}">編集</a>
-        <form method="post" action="{{ route('board.destroy', $post) }}">
+        <form method="post" action="{{ route('board.destroy', $post) }}" id="delete_post">
             @method('DELETE')
             @csrf
             <button class="btn">[x]</button>
         </form>
     </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
+
+    <script>
+        'use strict';
+
+        {
+            document.getElementById('delete_post').addEventListener('submit', e => {
+                e.preventDefault();
+
+                if (!confirm('本当に削除しますか？')) {
+                    return;
+                }
+
+                e.target.submit();
+            });
+        }
+    </script>
 </x-layout>
