5197e42d94faaa301b91c5f45a1141dc81f3ea2b
Suumi <ksksme@gmail.com>
2021-08-18 20:01:37 +0900

    ルーティングテスト

---
+++ backend/tests/Feature/RoutingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RoutingTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function ルーティングテスト()
+    {
+        $this->get(route('top'))->assertStatus(200);
+        $this->get(route('board.index'))->assertStatus(200);
+        $this->get(route('board.create'))->assertStatus(200);
+    }
+
+}
