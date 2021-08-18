f2c89538db05c45e5740b42f9c311879597b925b
Suumi <ksksme@gmail.com>
2021-08-18 18:08:05 +0900

    コード追加

--- backend/resources/views/components/layout.blade.php
+++ backend/resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>{{ $title }}</title>
         <link rel="stylesheet" href="{{ url('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div class="container">
